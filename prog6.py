59c91364bf0ab112ef3dea7372cea7c38426f93d
Elwena <Elwena@users.noreply.github.com>
2020-11-08 15:05:19 +0100

    zadanie 6

---
+++ prog6.py
@@ -0,0 +1,10 @@
+imie = 'Ala'
+ile = 2 #2 5
+zwierze = 'kot'
+
+if ile == 1:
+    print('{0} ma {1}a'.format(imie, zwierze))
+elif ile == 2:
+    print('{0} ma {1} {2}y'.format(imie, ile, zwierze))
+else:
+    print('{0} ma {1} {2}ow'.format(imie, zwierze))
