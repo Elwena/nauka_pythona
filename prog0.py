b50ba768a169ec105c09c773e276ab50759397ce
Elwena <Elwena@users.noreply.github.com>
2020-11-08 15:08:36 +0100

    zadanie?

---
+++ prog0.py
@@ -0,0 +1,9 @@
+print ('Hello Word')
+imie = 'Wioletta'
+nazwa = 'Terelak'
+ilosc_ksiazek = 10
+srednia = 4.5 # float
+
+print('Hello World' + imie + '  ' +nazwa)
+print('srednia: ' +str(srednia))
+print('ilosc ' + str(ilosc_ksiazek))
