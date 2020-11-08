dfaeeac6fdcade0175426aa0bba44ccffbb37711
Elwena <Elwena@users.noreply.github.com>
2020-11-08 15:07:56 +0100

    zadanie?

---
+++ prog1.py
@@ -0,0 +1,11 @@
+marka = 'Peugot'
+ilosc_drzwi = 5
+pojemnosc = 1.3
+
+marka_up = marka.upper()
+
+marka_up = marka.lower()
+
+print("Samochod " + marka + " ma " + str (ilosc_drzwi) + " drzwi")
+print(marka_up)
+print("Pojemnosc po zmianach: " + str (pojemnosc * 2))
