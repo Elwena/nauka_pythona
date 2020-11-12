c43bc5f342f7748be73948adb92ee364f59c6153
Wioletta Terelak <73889884+Elwena@users.noreply.github.com>
2020-11-12 15:13:36 +0100

    Update prog0.py

--- prog0.py
+++ prog0.py
@@ -7,3 +7,14 @@ srednia = 4.5 # float
 print('Hello World' + imie + '  ' +nazwa)
 print('srednia: ' +str(srednia))
 print('ilosc ' + str(ilosc_ksiazek))
+
+# na dole jest moj program
+
+gatunek_zwierzęcia = 'Kot'
+ilosc_łap = 4
+ilosc_posiłków = 4.5
+
+print ('Cześć ' + gatunek_zwierzęcia+ 'ku')
+input('Podaj imie kota:')
+print('Kot ma: ' +str(ilosc_łap) + ' łapy')
+print('zjada ' + str(ilosc_posiłków) + ' posilki')
